94c3b0abee6d5b7fc8d65b66c71f5820c55d1d98
Manny <manny1449@gmail.com>
2025-03-27 22:07:25 -0400

    exercises done till #6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
@@ -14,11 +15,11 @@ app.listen(port, () => {
   console.log("Hello World");
 });
 
-//exercise2
+//exercise2 commandline args
 // const args = process.argv.slice(2);
 // console.log("args =", args);
 
-// exercise3
+// exercise3 simple calc
 // const args = process.argv.slice(2);
 // const [num1, operator, num2] = args;
 
@@ -48,18 +49,45 @@ app.listen(port, () => {
 
 // console.log(`Result: ${result}`);
 
-// exercise4
-const args = process.argv.slice(1);
-const [input] = args;
+// exercise4 number guess
+// const args = process.argv.slice(1);
+// const [input] = args;
 
-const computerChoice = [Math.floor(Math.random() * 100)];
+// const computerChoice = [Math.floor(Math.random() * 100)];
 
-if (input === computerChoice) {
-  console.log("You Win!");
-  console.log("computerChoice =", computerChoice);
-  console.log("input =", input);
-} else {
-  console.log("You Lose!");
-  console.log("computerChoice =", computerChoice);
-  console.log("input =", input);
+// if (input === computerChoice) {
+//   console.log("You Win!");
+//   console.log("computerChoice =", computerChoice);
+//   console.log("input =", input);
+// } else {
+//   console.log("You Lose!");
+//   console.log("computerChoice =", computerChoice);
+//   console.log("input =", input);
+// }
+
+// exercise5 palindrome check
+// const args = process.argv.slice(2);
+// const [input] = args;
+// console.log("input =", input);
+// const revInput = [...input].reverse().join("");
+// console.log("revInput=", revInput);
+// const answer =
+//   JSON.stringify(input) === JSON.stringify(revInput)
+//     ? "Palindrome"
+//     : "Not a Palindrome";
+// console.log(answer);
+
+// exercise6 file reader
+const filePath = process.argv[2];
+if (!filePath) {
+  console.log("invalid filepath as argument");
+  process.exit(1);
 }
+
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error has occured", err);
+    process.exit(1);
+  }
+  console.log(data);
+});
